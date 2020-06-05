137cbc292018e5713aad79a8b8ae6cc5e1575caa
Rahul Sarathy <rahul@sarathy.org>
2020-06-04 20:13:32 -0700

    publishing fix

--- frontend/code_tutorial_nextjs/components/AddStep.tsx
+++ frontend/code_tutorial_nextjs/components/AddStep.tsx
@@ -1,28 +1,32 @@
 import React, { useState } from "react";
 const descriptionStyles = require("../styles/AddStep.module.scss");
-import Step from "./Step.tsx"
+import Step from "./Step";
 
 export default function AddStep() {
-
   const [clicked, onceClicked] = useState(false);
+
   const handleClick = () => {
     onceClicked(!clicked);
   };
 
+  const [published, publishText] = useState(false);
+
   return (
     <div className={descriptionStyles.steps}>
       <div className={descriptionStyles.header}>
-        <h1>
-        Add
-        </h1>
-        <div className={descriptionStyles.stepButtons}>
-          <div onClick={handleClick}>
-            {!clicked && <button className={descriptionStyles.step}>New Step</button>}
-            {!clicked && <button className={descriptionStyles.desc}>New Description</button>}
+        <h1>Add</h1>
+        <div className={descriptionStyles.stepArea}>
+          <div className={descriptionStyles.stepButtons} onClick={handleClick}>
+            {!clicked && (
+              <button className={descriptionStyles.step}>New Step</button>
+            )}
+            {!clicked && (
+              <button className={descriptionStyles.desc}>
+                New Description
+              </button>
+            )}
           </div>
-          <div>
-            {clicked && <Step />}
-          </div>
+          <div>{clicked && <Step clicked={clicked} />}</div>
         </div>
       </div>
     </div>
--- frontend/code_tutorial_nextjs/components/Step.tsx
+++ frontend/code_tutorial_nextjs/components/Step.tsx
@@ -1,35 +1,48 @@
 import React, { useState } from "react";
 const descriptionStyles = require("../styles/Step.module.scss");
 
-export default function Step() {
-  const [text, onceWritten] = useState('');
+type StepProps = {
+  clicked: boolean;
+};
 
-  const handleChange = (event) => {
-    onceWritten(event.target.value);
+export default function Step(props: StepProps) {
+  const [text, onceWritten] = useState("");
+  const [published, publish] = useState(false);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    publish(true);
   };
 
-  const handleSubmit = (event) => {
-    
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onceWritten(event.target.value);
   };
 
   return (
     <div className={descriptionStyles.stepForm}>
-      <form onSubmit={handleSubmit}>
-        <div className={descriptionStyles.header}>
-          <label className={descriptionStyles.titleName}>
-            Enter Here:
-            <textarea className={descriptionStyles.textarea} value={text} onChange={handleChange} />
-          </label>
-        </div>
-        <div className={descriptionStyles.stepButtons}>
-          <input className={descriptionStyles.done} type="submit" value="Done" />
-        </div>
-      </form>
+      {!published && (
+        <form onSubmit={handleSubmit}>
+          <div className={descriptionStyles.header}>
+            <label className={descriptionStyles.titleName}>
+              Enter Here:
+              <textarea
+                className={descriptionStyles.textarea}
+                value={text}
+                onChange={handleChange}
+              />
+            </label>
+          </div>
+          <div className={descriptionStyles.stepButtons}>
+            <input
+              className={descriptionStyles.done}
+              type="submit"
+              value="Done"
+            />
+          </div>
+        </form>
+      )}
       <div>
-        <p>
-          {text}
-        </p>
+        <p>{text}</p>
       </div>
     </div>
-    );
+  );
 }
