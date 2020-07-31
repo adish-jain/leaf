747a860b8fcce1632b281abf4c87e3d715baa7b6
Adish Jain <adish.jain@berkeley.edu>
2020-07-30 22:21:59 -0700

    Front page (#43)
    * basic structure of index page
    
    * flexy
    
    * basic form
    
    * asymmetric layout
    
    * working diagram
    
    * diagram done
    
    * cleaning up
    
    * front page done
    
    * route about to login for now
    
    * remove comment
    
    * changed fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import { useLoggedIn, logOut } from "../lib/UseLoggedIn";
 import { useRouter } from "next/router";
 
 const appStyles = require("../styles/App.module.scss");
+const indexStyles = require("../styles/Index.module.scss");
 
 export default function Pages() {
   const router = useRouter();
@@ -16,7 +17,7 @@ export default function Pages() {
 
   const handleClick = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
-    router.push("/article");
+    router.push("/login");
   };
 
   return (
@@ -35,32 +36,220 @@ export default function Pages() {
         />
       </Head>
       <main>
-        <div>
-          <Header />
-          <div className={appStyles.Landing}>
-            <h1>
-              This is a platform to view coding tutorials in a better format.
-            </h1>
-            <h3>Check out an example tutorial</h3>
-            <div onClick={handleClick} className={appStyles.Preview}>
-              <h2>Example Tutorial</h2>
-            </div>
-          </div>
-        </div>
+        <Header />
+        <Body handleClick={handleClick}/>
       </main>
     </div>
   );
 }
 
+/*
+Header Components
+*/
 function Header() {
   return (
-    <div className={appStyles.Header}>
+    <div className={indexStyles.Header}>
+      <Logo />
+      <HeaderText />
+      <Login />
+      <Signup />
+      <About />
+
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div className={indexStyles.Logo}> 
+      Leaf.
+    </div>  
+  );
+}
+
+function Login() {
+  return (
+    <div className={indexStyles.Login}>
       <Link href="/login">
         <a>Login</a>
       </Link>
+    </div>
+  );
+}
+
+function Signup() {
+  return (
+    <div className={indexStyles.Login}>
       <Link href="/signup">
         <a>Signup</a>
       </Link>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div className={indexStyles.Login}>
+      <Link href="/login">
+        <a>About</a>
+      </Link>
+    </div>
+  );
+}
+
+function HeaderText() {
+  return (
+    <div>
+      <div className={indexStyles.HeaderText}>
+          A New Way to Convey
+      </div>
+      <GetStarted />
+    </div>
+  );
+}
+
+function GetStarted() {
+  return (
+      <Link href="/signup">
+        <div>
+        <div className={indexStyles.GetStarted}>
+          <div className={indexStyles.button}>
+            Get Started
+          </div>
+        </div>
+        </div>
+      </Link>
+  );
+}
+
+/*
+Body Components
+*/
+function Body(props: {handleClick: any}) {
+  return (
+    <div className={indexStyles.Body}> 
+      <BodyBox />
+      <BodyText handleClick={props.handleClick}/>
+    </div>
+  );
+}
+
+function BodyText(props: {handleClick: any}) {
+  return (
+      <div>
+        <div className={indexStyles.BodyTextH1}>
+          Coding tutorials <br></br> to the next level<br></br> 
+        </div>
+        <div className={indexStyles.BodyTextH2}>
+           Steps & code editor <br></br> side-by-side for a <br></br> seamless experience
+        </div>
+        <div onClick={props.handleClick} className={indexStyles.Preview}>
+          <h2>Example</h2>
+        </div>
+      </div>
+  );
+}
+
+function BodyBox() {
+  return (
+    <div>
+      <div className={indexStyles.BodyBoxDiagram}>
+        <BodyBoxSteps />
+        <BodyBoxCode />
+      </div>
+    </div>
+  );
+}
+
+function BodyBoxSteps () {
+  return (
+    <div className={indexStyles.BodyBoxLeft}>
+      <UnfocusedStep />
+      <FocusedStep />
+      <UnfocusedStep />
+    </div>
+  );
+}
+
+function UnfocusedStep () {
+  return (
+    <div className={indexStyles.BodyBoxUnfocusedBox}>
+      <div className={indexStyles.Description1}></div>
+      <div className={indexStyles.Description2}></div>
+      <div className={indexStyles.Description3}></div>
+    </div>
+  );
+}
+
+function FocusedStep () {
+  return (
+    <div className={indexStyles.BodyBoxFocusedBox}>
+      <div className={indexStyles.Description1}></div>
+      <div className={indexStyles.Description2}></div>
+      <div className={indexStyles.Description3}></div>
+    </div>
+  );
+}
+
+function BodyBoxCode() {
+  return (
+    <div className={indexStyles.BodyBoxRight}>
+      <FileHeader />
+      <UnfocusedFunction />
+      <FocusedFunction />
+      <UnfocusedFunction />
+      <LanguageFooter />
+    </div>
+  );
+}
+
+function FileHeader () {
+  return (
+    <div>
+      <div className={indexStyles.File}>
+        test.jsx
+      </div>
+      <div className={indexStyles.FileBar}>
+      </div>
+    </div>
+  );
+}
+
+function LanguageFooter () {
+  return (
+    <div>
+      <div className={indexStyles.LanguageBar}></div>
+      <div className={indexStyles.Language}>
+        JSX
+      </div>
+    </div>
+  )
+}
+ 
+function UnfocusedFunction () {
+  return (
+    <div>
+      <div className={indexStyles.functionSignatureUnfocused}></div>
+      <div className={indexStyles.firstLineUnfocused}></div>
+      <div className={indexStyles.firstLineUnfocused}></div>
+      <div className={indexStyles.ifUnfocused}></div>
+      <div className={indexStyles.ifBlockUnfocused}></div>
+      <div className={indexStyles.ifUnfocused}></div>
+      <div className={indexStyles.ifBlockUnfocused}></div>
+    </div>
+  );
+}
+
+function FocusedFunction () {
+  return (
+    <div>
+      <div className={indexStyles.functionSignatureFocused}></div>
+      <div className={indexStyles.firstLineFocused}></div>
+      <div className={indexStyles.firstLineFocused}></div>
+      <div className={indexStyles.ifFocused}></div>
+      <div className={indexStyles.ifBlockFocused}></div>
+      <div className={indexStyles.elseFocused}></div>
+      <div className={indexStyles.elseBlockFocused}></div>
     </div>
   );
 }
