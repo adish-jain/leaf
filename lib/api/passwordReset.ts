69981b3fe1741c446f6b3c54c3d2fc519f54e3e6
Adish <adish.jain@berkeley.edu>
2020-08-26 19:28:44 -0700

    ready for review

--- lib/api/passwordReset.ts
+++ lib/api/passwordReset.ts
@@ -50,56 +50,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             console.log("signed out success");
           },
           function (error: any) {
-            console.log(error);
             // An error happened.
+            console.log(error);
           }
         );
     } catch (error) {
       var errorMessage = error.message;
       console.log(errorMessage);
+      res.status(403).send({
+        error: "Password Reset failed."
+      })
+      return;
     }
-    // .then(async function (firebaseUser: any) {
-    //   console.log("sending pw reset email");
-    //   await firebase.auth().sendPasswordResetEmail(email)
-    //   .catch(function (error: any) {
-    //     switch (error.code) {
-    //       case "auth/invalid-email":
-    //         res.status(403).send({
-    //           error: "Email is badly formatted.",
-    //         });
-    //         return;
-    //       case "auth/user-not-found":
-    //         res.status(403).send({
-    //           error: "This email doesn't exist in the Leaf system.",
-    //         });
-    //         return;
-    //       default:
-    //         console.log(error);
-    //         res.status(403).send({
-    //           error: "Password Reset failed",
-    //         });
-    //     }
-    //   });
-    //   firebase
-    //     .auth()
-    //     .signOut()
-    //     .then(
-    //       function () {
-    //         // Sign-out successful.
-    //         console.log("signed out success");
-    //       },
-    //       function (error: any) {
-    //         console.log(error);
-    //         // An error happened.
-    //       }
-    //     );
-    // })
-    // .catch(function (error: any) {
-    //   // Handle Errors here.
-    //   var errorCode = error.code;
-    //   var errorMessage = error.message;
-    //   console.log(errorMessage);
-    // });
     res.status(200);
     res.end();
     return;
