084ec8ece4ce68dc75316b4264772515dae8e7e0
Rahul Sarathy <rahul@sarathy.org>
2020-08-31 04:20:06 -0700

    functioning scroll bar

--- components/PublishedFileBar.tsx
+++ components/PublishedFileBar.tsx
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, useRef, useState, useEffect } from "react";
 const fileBarStyles = require("../styles/FileBar.module.scss");
+import animateScrollTo from "animated-scroll-to";
 
 type File = {
   id: string;
@@ -15,17 +16,82 @@ type PublishedFileBarProps = {
 };
 
 export default function PublishedFileBar(props: PublishedFileBarProps) {
+  const fileBarWrapperRef = React.useRef<HTMLDivElement>(null);
+  const fileBarRef = React.useRef<HTMLDivElement>(null);
+  const [scrollValue, updateScrollValue] = useState(0);
+
+  useEffect(() => {
+    const options = {
+      elementToScroll: fileBarWrapperRef.current!,
+    };
+    animateScrollTo([scrollValue, null], options);
+  }, [scrollValue]);
+
+  function showRight() {
+    let width = fileBarWrapperRef.current?.offsetWidth!;
+    let total_width = fileBarRef.current?.scrollWidth!;
+    if (!(scrollValue + width >= total_width)) {
+      updateScrollValue(scrollValue + width);
+    } else {
+      updateScrollValue(total_width);
+    }
+  }
+
+  function showLeft() {
+    let width = fileBarWrapperRef.current?.offsetWidth!;
+    if (scrollValue > 0) {
+      updateScrollValue(scrollValue - width);
+    }
+  }
+
+  function LeftButton() {
+    if (scrollValue === 0) {
+      return <div></div>;
+    }
+    return (
+      <button
+        onClick={showLeft}
+        className={fileBarStyles["published-filebar-left"]}
+      >
+        {"<"}
+      </button>
+    );
+  }
+
+  function RightButton() {
+    let total_width = fileBarRef.current?.scrollWidth!;
+    let width = fileBarWrapperRef.current?.offsetWidth!;
+    if (scrollValue + width >= total_width) {
+      return <div></div>;
+    }
+    return (
+      <button
+        onClick={showRight}
+        className={fileBarStyles["published-filebar-right"]}
+      >
+        {">"}
+      </button>
+    );
+  }
+
   return (
-    <div className={fileBarStyles["published-filebar"]}>
-      {props.files.map((file, index) => (
-        <File
-          key={file.id}
-          index={index}
-          name={file.name}
-          updateFile={props.updateFile}
-          selected={file.name === props.currentFile.name}
-        />
-      ))}
+    <div
+      ref={fileBarWrapperRef}
+      className={fileBarStyles["published-filebar-wrapper"]}
+    >
+      <LeftButton />
+      <div ref={fileBarRef} className={fileBarStyles["published-filebar"]}>
+        {props.files.map((file, index) => (
+          <File
+            key={file.id}
+            index={index}
+            name={file.name}
+            updateFile={props.updateFile}
+            selected={file.name === props.currentFile.name}
+          />
+        ))}
+      </div>
+      <RightButton />
     </div>
   );
 }
