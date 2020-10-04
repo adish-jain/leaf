10ab74d3a7c9c6b338b40fbc421c8dfc9d50a528
Adish <adish.jain@berkeley.edu>
2020-10-04 13:18:13 -0700

    get all images

--- lib/postUtils.ts
+++ lib/postUtils.ts
@@ -113,6 +113,37 @@ export async function getDraftDataFromPostId(username: string, postId: string) {
   return { ...otherDraftData, steps };
 }
 
+export async function getDraftImages(uid: string, draftId: string) {
+  let stepsRef = db
+    .collection("users")
+    .doc(uid)
+    .collection("drafts")
+    .doc(draftId)
+    .collection("steps")
+    .orderBy("order");
+
+  return await stepsRef
+    .get()
+    .then(function (stepsCollection: any) {
+      let results: any[] = [];
+      stepsCollection.forEach(function (result: any) {
+        let resultsJSON = result.data();
+        if (resultsJSON.imageURL !== undefined) {
+          resultsJSON.id = result.id;
+          results.push({
+            id: resultsJSON.id,
+            imageURL: resultsJSON.imageURL,
+          });
+        }
+      });
+      return results;
+    })
+    .catch(function (error: any) {
+      console.log(error);
+      return [];
+    });
+}
+
 type File = {
   id: string;
   language: string;
