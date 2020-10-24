4a6799451fc8bf1a67c8f7a173f739c7fb507253
Rahul Sarathy <rahul@sarathy.org>
2020-10-24 15:37:34 -0700

    dim lines

--- components/PrismEditor.tsx
+++ components/PrismEditor.tsx
@@ -1,4 +1,4 @@
-import Prism from "prismjs";
+import Prism, { highlight } from "prismjs";
 import React, { Component, createRef } from "react";
 import "../styles/prism.css";
 import "../styles/prism-white.css";
@@ -29,13 +29,17 @@ type PrismEditorState = {
   startHighlightLine?: number;
   endHighlightLine?: number;
   hideLines: boolean;
+  hovered: boolean;
 };
+let highlightedFiles: string[] = [];
 
 export default class PrismEditor extends Component<
   PrismEditorProps,
   PrismEditorState
 > {
   PrismWrapper = createRef<HTMLDivElement>();
+
+  linesWrapper = createRef<HTMLDivElement>();
 
   constructor(props: PrismEditorProps) {
     super(props);
@@ -44,11 +48,28 @@ export default class PrismEditor extends Component<
     this.animateLines = this.animateLines.bind(this);
     this.renderFiles = this.renderFiles.bind(this);
     this.highlightedLines = this.highlightedLines.bind(this);
+    this.renderFile = this.renderFile.bind(this);
+    this.Line = this.Line.bind(this);
+
+    this.syntaxHighlightFiles();
 
     this.state = {
       hideLines: false,
+      hovered: false,
     };
   }
+
+  syntaxHighlightFiles = () => {
+    let { files } = this.props;
+    for (let i = 0; i < files.length; i++) {
+      let highlightedCode = Prism.highlight(
+        files[i].code,
+        Prism.languages.typescript,
+        "typescript"
+      );
+      highlightedFiles.push(highlightedCode);
+    }
+  };
 
   componentDidUpdate(prevProps: PrismEditorProps) {
     let { steps, currentStepIndex, files, currentFileIndex } = this.props;
@@ -59,6 +80,12 @@ export default class PrismEditor extends Component<
     // if step changes
     if (previousStep.id !== currentStep?.id) {
       this.updateLines();
+    }
+
+    // file is deleted or added
+    if (prevProps.files.length != files.length) {
+      this.syntaxHighlightFiles();
+      console.log("file length updated");
     }
 
     // if file changes
@@ -82,7 +109,9 @@ export default class PrismEditor extends Component<
   }
 
   componentDidMount() {
-    Prism.highlightAll();
+    // Prism.highlightAll();
+    console.log("CDM called");
+    this.syntaxHighlightFiles();
     this.updateLines();
   }
 
@@ -128,15 +157,14 @@ export default class PrismEditor extends Component<
     let lineString = `${this.state.startHighlightLine}-${this.state.endHighlightLine}`;
 
     return (
-      <div>
+      <div style={{ background: "white" }} ref={this.linesWrapper}>
         {files.map((file, index) => {
           let style = {
             display: "none",
+            color: "black",
           };
           if (index === currentFileIndex) {
-            style = {
-              display: "block",
-            };
+            style.display = "block";
           }
           return (
             <pre
@@ -146,6 +174,7 @@ export default class PrismEditor extends Component<
             >
               <code className="language-tsx">{file.code}</code>
               <this.highlightedLines />
+              {/* <this.dimmer /> */}
             </pre>
           );
         })}
@@ -153,14 +182,18 @@ export default class PrismEditor extends Component<
     );
   }
 
-  highlightedLines() {
-    let { currentStepIndex, steps } = this.props;
+  highlightedLines = () => {
+    let { currentStepIndex, steps, currentFileIndex, files } = this.props;
     let currentStep = steps[currentStepIndex];
+    let currentFile = files[currentFileIndex];
+    if (currentFile.id !== currentStep.fileId) {
+      return <div></div>;
+    }
+
     let lines = currentStep.lines;
     if (lines == null) {
       return <div></div>;
     }
-    console.log(lines);
     let height = 1.5 * (Math.abs(lines.start - lines.end) + 1);
     let top = 1.5 * (lines.start - 1);
     let lineStyles = {
@@ -168,7 +201,107 @@ export default class PrismEditor extends Component<
       top: `${top}em`,
     };
     return <div style={lineStyles} className={"highlighted-lines"}></div>;
+  };
+
+  renderFile() {
+    let { steps, currentStepIndex, files, currentFileIndex } = this.props;
+    let currentFile = files[currentFileIndex];
+    let lines = highlightedFiles[currentFileIndex].split(/\r?\n/);
+    return <this.CodeFile index={1} key={currentFile.id} lines={lines} />;
+    // return (
+    //   <div>
+    //     {highlightedFiles.map((file, index) => {
+    //       let lines = file.split(/\r?\n/);
+    //       return <this.CodeFile key={index} index={index} lines={lines} />;
+    //     })}
+    //   </div>
+    // );
   }
+
+  CodeFile = (props: { lines: string[]; index: number }) => {
+    let lines = props.lines;
+    let index = props.index;
+    let { hovered } = this.state;
+    let { steps, currentStepIndex, files, currentFileIndex } = this.props;
+    let style = {
+      background: "white",
+      display: "block",
+    };
+
+    return (
+      <div>
+        <pre style={style} className="line-numbers">
+          <code className="language-tsx">
+            {lines.map((line, index) => (
+              <this.Line
+                steps={steps}
+                currentStepIndex={currentStepIndex}
+                key={`Line-${index}`}
+                index={index}
+                line={line}
+                files={files}
+                currentFileIndex={currentFileIndex}
+                hovered={hovered}
+              />
+            ))}
+          </code>
+        </pre>
+      </div>
+    );
+  };
+
+  Line(LineProps: {
+    index: number;
+    line: string;
+    steps: Step[];
+    currentStepIndex: number;
+    files: File[];
+    currentFileIndex: number;
+    hovered: boolean;
+  }) {
+    let {
+      index,
+      line,
+      steps,
+      currentStepIndex,
+      files,
+      currentFileIndex,
+      hovered,
+    } = LineProps;
+    let currentStep = steps[currentStepIndex];
+    let currentFile = files[currentFileIndex];
+    let lines = currentStep.lines;
+    let highlighted = "";
+    let line_dim = "";
+    let differentFile = currentFile.id !== currentStep.fileId;
+    if (
+      lines &&
+      index >= lines.start - 1 &&
+      index <= lines.end - 1 &&
+      !differentFile
+    ) {
+      highlighted = "highlighted";
+    } else if (!hovered && lines) {
+      line_dim = "line-dim";
+    }
+    return (
+      <div className={`line-wrapper ${highlighted} ${line_dim}`}>
+        <span className={"line-number"}>{index}</span>
+        <div
+          className={"line-content"}
+          dangerouslySetInnerHTML={injectHTML(line + "\n")}
+        ></div>
+      </div>
+    );
+  }
+
+  handleMouseEnter = () => {
+    this.setState({ hovered: true });
+  };
+
+  handleMouseLeave = () => {
+    this.setState({ hovered: false });
+  };
 
   render() {
     let { steps, currentStepIndex, files, currentFileIndex } = this.props;
@@ -178,9 +311,19 @@ export default class PrismEditor extends Component<
     let { language } = currentFile;
 
     return (
-      <div ref={this.PrismWrapper} className={"prism-editor"}>
-        <this.renderFiles />
+      <div
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+        ref={this.PrismWrapper}
+        className={"prism-editor"}
+      >
+        {/* <this.renderFiles /> */}
+        <this.renderFile />
       </div>
     );
   }
+}
+
+function injectHTML(line: string) {
+  return { __html: line };
 }
