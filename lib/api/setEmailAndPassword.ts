198bd978f0c50252e7ef965e01dc7e77b33d0792
Adish <adish.jain@berkeley.edu>
2020-11-11 20:18:11 -0800

    resolve email/pw set issue

--- lib/api/setEmailAndPassword.ts
+++ lib/api/setEmailAndPassword.ts
@@ -14,7 +14,7 @@ export default async function setEmailAndPasswordHandler(
 ) {
   let newPassword = req.body.newPassword;
   let newEmail = req.body.newEmail;
-  let errored = false;
+  var errored = false;
 
   let { uid, userRecord } = await getUser(req, res);
 
@@ -74,46 +74,63 @@ export default async function setEmailAndPasswordHandler(
   await firebase
   .auth()
   .signInWithCustomToken(customToken)
-  .then(function (firebaseUser: any) {
-    let user = firebaseUser.user;
-    admin
-    .auth()
-    .updateUser(uid, {
-        email: newEmail,
-        emailVerified: false,
-        password: newPassword
-    })
-    .catch(function(error: any) {
-        switch (error.code) {
-            case "auth/invalid-password":
-                res.status(403).send({
-                    error: "Password should be longer than 6 characters.",
-                });
-                return;
-            case "auth/invalid-email":
-                res.status(403).send({
-                    error:
-                    "Email address is poorly formatted.",
-                });
-                errored = true;
-                return;
-            case "auth/email-already-exists":
-                res.status(403).send({
-                    error: "Email already in use by another account.",
-                });
-                errored = true;
-                return;
-            default:
-                console.log(error);
-                res.status(403).send({
-                    error: "Reset failed",
-                });
-                errored = true;
-                return;
-        }
+  .then(async function (firebaseUser: any) {
+        let user = firebaseUser.user;
+        admin
+        .auth()
+        .updateUser(uid, {
+            email: newEmail,
+            emailVerified: false,
+            password: newPassword
+        })
+        .then(async function () {
+            await db.collection("users").doc(uid).set(
+                {
+                email: newEmail,
+                method: "leaf",
+                },
+                { merge: true }
+            );
+            let userDataReference = await db.collection("users").doc(uid).get();
+            let userData = await userDataReference.data();
+            console.log(userData);
+            res.status(200).end();  
+            return;
+        })
+        .catch(function(error: any) {
+            switch (error.code) {
+                case "auth/invalid-password":
+                    res.status(403).send({
+                        error: "Password should be longer than 6 characters.",
+                    });
+                    errored = true;
+                    return;
+                case "auth/invalid-email":
+                    console.log("We caught the error");
+                    res.status(403).send({
+                        error:
+                        "Email address is poorly formatted.",
+                    });
+                    errored = true;
+                    return;
+                case "auth/email-already-exists":
+                    res.status(403).send({
+                        error: "Email already in use by another account.",
+                    });
+                    errored = true;
+                    return;
+                default:
+                    console.log(error);
+                    res.status(403).send({
+                        error: "Reset failed",
+                    });
+                    errored = true;
+                    return;
+            }
+        })
     });
 
-    firebase
+    await firebase
       .auth()
       .signOut()
       .then(function () {
@@ -122,19 +139,6 @@ export default async function setEmailAndPasswordHandler(
       })
       .catch(function(error: any) {
       });
-  });
-
-  await db.collection("users").doc(uid).set(
-    {
-      email: newEmail,
-      method: "leaf",
-    },
-    { merge: true }
-  );
-
-  if (!errored) {
-    res.status(200).end();  
-  }
 
   return;
 }
