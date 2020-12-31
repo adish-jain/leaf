b51915672d2f76623fcb07fed5cb7b4517974d93
Rahul Sarathy <rahul@sarathy.org>
2020-12-30 23:04:02 -0800

    fix mobile issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,8 @@ import "../styles/prism-atom-dark.css";
 
 import { AppProps } from "next/app";
 import * as Sentry from "@sentry/node";
-import { useWindowSize } from "@react-hook/window-size";
+// import { useWindowSize } from "@react-hook/window-size";
+import { useWindowSize } from "../lib/useWindowSize";
 import { DimensionsContext } from "../contexts/dimensions-context";
 
 if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
@@ -18,13 +19,13 @@ if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
 export const MOBILE_WIDTH = 450;
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [width, height] = useWindowSize();
+  const windowSize = useWindowSize();
 
   return (
     <DimensionsContext.Provider
       value={{
-        height: height,
-        width: width,
+        height: windowSize.height || 0,
+        width: windowSize.width || 0,
       }}
     >
       <Component {...pageProps} />
